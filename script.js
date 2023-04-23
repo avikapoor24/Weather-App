02693b953eb4c1b9f1f485272c4b6ee4f5b839e1
avikapoor24 <15.avikapoor@gmail.com>
2023-04-23 22:57:50 +0530

    commit 2
    Signed-off-by: avikapoor24 <15.avikapoor@gmail.com>

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
 const options = {
-	method: 'GET',
-	headers: {
-		'content-type': 'application/octet-stream',
-		'X-RapidAPI-Key': '53aaf81232msh377624d3b3e6134p145931jsn90b446a95ec5',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'content-type': 'application/octet-stream',
+        'X-RapidAPI-Key': '53aaf81232msh377624d3b3e6134p145931jsn90b446a95ec5',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
 };
 
 // try {
 // 	const response = await fetch(url, options);
 // 	const result = await response.text();
-// 	console.log(result);
+// 	console.log(result); ̰
 // } catch (error) {
 // 	console.error(error);
 // }
@@ -20,7 +20,16 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle', op
     .then(response => response.json())
     .then(response => {
 
-        
+        cloud_pct.innerHTML = response.cloud_pct
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        max_temp.innerHTML = response.max_temp
+        min_temp.innerHTML = response.min_temp
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+        temp.innerHTML = response.temp
+        wind_degrees.innerHTML = response.wind_degrees
+        wind_speed.innerHTML = response.wind_speed
 
 
         console.log(response)
