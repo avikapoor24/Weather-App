4f6fb541a1fb12b81ee9fd7c15c9f161bdc1bdd6
avikapoor24 <avikapoor244@gmail.com>
2023-04-24 01:19:24 +0530

    Commit 4
    Signed-off-by: avikapoor24 <avikapoor244@gmail.com>

--- script.js
+++ script.js
@@ -15,24 +15,33 @@ const options = {
 // } catch (error) {
 // 	console.error(error);
 // }
+const getWeather = (city) => {
+    cityName.innerHTML= city 
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi', options)
+        .then(response => response.json())
+        .then((response) => {
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi', options)
-    .then(response => response.json())
-    .then((response) => {
-
-        cloud_pct.innerHTML = response.cloud_pct
-        feels_like.innerHTML = response.feels_like
-        humidity.innerHTML = response.humidity
-        max_temp.innerHTML = response.max_temp
-        min_temp.innerHTML = response.min_temp
-        sunrise.innerHTML = response.sunrise
-        sunset.innerHTML = response.sunset
-        temp.innerHTML = response.temp
-        wind_degrees.innerHTML = response.wind_degrees
-        wind_speed.innerHTML = response.wind_speed
+            cloud_pct.innerHTML = response.cloud_pct
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            max_temp.innerHTML = response.max_temp
+            min_temp.innerHTML = response.min_temp
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+            temp.innerHTML = response.temp
+            wind_degrees.innerHTML = response.wind_degrees
+            wind_speed.innerHTML = response.wind_speed
 
 
-        console.log(response)
-    }
-    )
-    .catch(err => console.error(err));
+            console.log(response)
+        }
+        )
+        .catch(err => console.error(err));
+}
+
+submit.addEventListener("click", () => {
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather("Delhi")
